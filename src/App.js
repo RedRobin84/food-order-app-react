d53a275de99525f556a73e452ec3761f8cebbdab
Martin Červenka <martin.cervenka@rohlik.cz>
2022-02-09 10:03:55 +0100

    add cart context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import {Fragment, useState} from 'react';
-import Cart from './components/Cart/Cart';
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
-import Meals from './components/Meals/Meals';
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [showCart, setShowCart] = useState(false);
 
   const showCartHandler = () => {
     setShowCart(true);
-  }
+  };
   const hideCartHandler = () => {
     setShowCart(false);
-  }
+  };
 
   return (
-    <Fragment>
+    <CartProvider>
       {showCart && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
